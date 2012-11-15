ce728cab7a35999fc734e500a3404c091249a632
Mark Guerra <markgx@gmail.com>
2012-11-15 15:46:44 -0800

    programatically show tab

--- tabs.js
+++ tabs.js
@@ -10,20 +10,29 @@
 
       $('a', this.$el).on('click', function (e) {
         e.preventDefault();
-
-        var $target = $(this);
-
-        $('a.active', _this.$el).removeClass('active');
-        $target.addClass('active');
-
-        var paneID = $target.data('pane');
-        $('.tab-content .tab-pane').hide();
-        $('.tab-content #' + paneID + '.tab-pane').show();
+        _this.showTab(this);
       });
+    },
+
+    showTab: function(target) {
+      var $target = $(target);
+
+      $('a.active', this.$el).removeClass('active');
+      $target.addClass('active');
+
+      var paneID = $target.data('pane');
+      $('.tab-content .tab-pane').hide();
+      $('.tab-content #' + paneID + '.tab-pane').show();
     }
   };
 
   $.fn.tabs = function(options) {
+    if (options === 'show') {
+      var $parent = $(this).parents('ul').first();
+      var obj = $parent.data('tabs');
+      obj.showTab(this);
+    }
+
     if (this.length) {
       this.each(function() {
         var obj = new Tabs(this, options);
